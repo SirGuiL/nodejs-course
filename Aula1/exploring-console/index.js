72b6f0d24c24c91a9dfd26a29a4b0f40e8a9b04d
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2023-03-18 20:43:51 -0300

    exploring console

---
+++ Aula1/exploring-console/index.js
@@ -0,0 +1,20 @@
+const x = 10;
+const y = "text";
+const z = [1, 2];
+
+console.log(x, y, z);
+
+console.count('contagem');
+console.count('contagem');
+console.count('contagem');
+console.count('contagem');
+console.count('contagem');
+console.count('contagem');
+console.count('contagem');
+console.count('contagem');
+
+console.log('%s', y)
+
+setTimeout(() => {
+  console.clear();
+}, 2000)
