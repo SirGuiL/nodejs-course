9834957f13bc07b9dac6dcb6a6396473460a0cc4
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2023-05-20 21:31:46 -0300

    route modules

--- Aula4/index.js
+++ Aula4/index.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 const path = require("path");
 
-const basePath = path.join(__dirname, "templates");
+const users = require('./users');
 
 // read body
 app.use(express.urlencoded({
@@ -13,37 +13,9 @@ app.use(express.urlencoded({
 
 app.use(express.json());
 
-// const checkAuth = (req, res, next) => {
-//   req.authStatus = true;
+const basePath = path.join(__dirname, "templates");
 
-//   if (req.authStatus) {
-//     console.log('está logado')
-//     next();
-//   } else {
-//     console.log('não está logado')
-//     next();
-//   }
-// }
-
-// app.use(checkAuth)
-
-app.get("/users/add", (req, res) => {
-  res.sendFile(`${basePath}/userform.html`);
-});
-
-app.post("/users/save", (req, res) => {
-  const name = req.body.name;
-  const age = req.body.age;
-
-  console.log(`${name} has ${age} years old`);
-});
-
-app.get("/users/:id", (req, res) => {
-  const id = req.params.id
-
-  console.log(id)
-  res.sendFile(`${basePath}/users.html`);
-});
+app.use('/users', users);
 
 app.get("/", (req, res) => {
   res.sendFile(`${basePath}/index.html`);
