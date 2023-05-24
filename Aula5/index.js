4e7e9b4cd939d0b4179ed18756b2cdb98a8b0c51
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2023-05-24 19:05:50 -0300

    send data

--- Aula5/index.js
+++ Aula5/index.js
@@ -7,9 +7,13 @@ app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
-  res.render("home");
+  const user = {
+    name: "Guilherme",
+    lastname: "Lima",
+  };
+  res.render("home", { user });
 });
 
 app.listen(3000, () => {
-  console.log('listen on localhost:3000');
+  console.log("listen on localhost:3000");
 });
